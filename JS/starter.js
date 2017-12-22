fbd0aa23a0725c23eb65543390bda2a4b60ecc94
MikeJalow <jalo0011@algonquinlive.com>
2017-12-21 23:15:06 -0500

    added code

---
+++ JS/starter.js
@@ -0,0 +1,5 @@
+let originalList = [
+    {"fullname":"Michael Scott","phone":"613-613-6113","email":"thebestboss@dundermifflin.com"}
+    {"fullname":"Dwight K. Schrute","phone":"No number on record","email":"BearsAreBest@dundermifflin.com"}
+    {"fullname":"Pam Beesly","phone":"613-531-2664","email":"PamelaB@gmail.com"}
+]
